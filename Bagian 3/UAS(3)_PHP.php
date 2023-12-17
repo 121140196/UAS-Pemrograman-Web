016084655287cb7bc8fcf6541f8321dd49db72e4
121140196_ Adriel Gideon Benedict Sirait <126436518+121140196@users.noreply.github.com>
2023-12-18 06:41:39 +0700

    Update UAS(3)_PHP.php

--- Bagian 3/UAS(3)_PHP.php
+++ Bagian 3/UAS(3)_PHP.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root'); 
 define('DB_PASSWORD', ''); 
-define('DB_NAME', 'uas_pemweb'); 
+define('DB_NAME', 'uas_pemrograman_web'); 
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
