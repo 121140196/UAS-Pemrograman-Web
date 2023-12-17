9b03eb384333648210357e20c0f268d224aa4d84
121140196_ Adriel Gideon Benedict Sirait <126436518+121140196@users.noreply.github.com>
2023-12-18 06:35:02 +0700

    Add files via upload

---
+++ Bagian 1/UAS(1)_JavaScript.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    document.getElementById('dataForm').addEventListener('submit', function (event) {
+      event.preventDefault(); 
+  
+      if (validateForm()) {
+        processData();
+      }
+    });
+  
+    document.getElementById('hobi').addEventListener('change', function () {
+      alert('Hobi terpilih');
+    });
+  
+    function validateForm() {
+      var name = document.getElementById('name').value;
+      var umur = document.getElementById('umur').value;
+  
+      if (name === '' || umur === '') {
+        alert('Name and Umur harus dipilih');
+        return false;
+      }
+  
+      return true;
+    }
+  
+    function processData() {
+      var name = document.getElementById('name').value;
+      var umur = document.getElementById('umur').value;
+      var hobi = document.getElementById('hobi').checked ? 'Membaca' : 'Menulis';
+      var jeniskelamin = document.querySelector('input[name="kelas"]:checked').value;
+  
+      var newRow = document.createElement('tr');
+  
+      newRow.innerHTML = `
+        <td>${name}</td>
+        <td>${umur}</td>
+        <td>${hobi}</td>
+        <td>${jeniskelamin}</td>
+      `;
+  
+      document.getElementById('dataTable').getElementsByTagName('tbody')[0].appendChild(newRow);
+  
+      document.getElementById('dataForm').reset();
+    }
+});
+  
